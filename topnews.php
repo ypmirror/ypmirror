dc747dd51f33d9fcae4fa0f6565f98663f622a66
Derpy Hooves <bestpony@equestria.mlp>
2015-12-24 03:15:01 -0700

    Derpy is best pony. No exceptions.

--- topnews.php
+++ topnews.php
@@ -1,1 +1,1 @@
-<div id=msg><strong>Yay Steven Universe is LIVE!  =><a href="/yaysu/" style="color:white"> Visit it!</a></strong></div>
+<div id=msg><strong>We're running out of iTunes credit for the comics again, If you can, <br />please send us your iTunes US gift card codes at <a href="mailto:contact@yayponies.eu" style="color:white">contact@yayponies.eu</a></strong></div>
