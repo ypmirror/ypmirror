73c96a75aa5d25bc4d573b3a2404f7addbb33763
Derpy Hooves <bestpony@equestria.mlp>
2015-12-24 09:30:02 -0700

    Derpy is best pony. No exceptions.

--- topnews.php
+++
@@ -1,1 +0,0 @@
-<div id=msg><strong>We're running out of iTunes credit for the comics again, If you can, <br />please send us your iTunes US gift card codes at <a href="mailto:contact@yayponies.eu" style="color:white">contact@yayponies.eu</a></strong></div>
