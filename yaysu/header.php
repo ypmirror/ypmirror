3e6e63c15056dfa8053cda4a67844cb6fe01d367
Derpy Hooves <bestpony@equestria.mlp>
2016-05-28 02:31:02 -0600

    Derpy is best pony. No exceptions.

--- yaysu/header.php
+++ yaysu/header.php
@@ -20,5 +20,5 @@ content="Steven Universe, episodes, download, Torrent, Direct Download, iTunes, 
   <div id="container">
   <center><small id="small"><a href=http://fav.me/d8uob8w>Steven Universe by DashieSparkle</a></small></center><br />
 <center> 
-  <p><big><a href="index.php">Index</a></big> <big>| <a href="FAQ.php">FAQ</a></big> <big> | <a href="steven.php">Episodes</a> | <a href="https://twitter.com/#!/YayPonies_eu">Twitter</a> | <a href="/">YayPonies</a></big></p>
+  <p><big><a href="index.php">Index</a></big> <big>| <a href="FAQ.php">FAQ</a></big> <big> | <a href="steven.php">Episodes</a> | <a href="rss/raw.rss">RSS</a> | <a href="https://twitter.com/#!/YayPonies_eu">Twitter</a> | <a href="/">YayPonies</a></big></p>
 </center>
