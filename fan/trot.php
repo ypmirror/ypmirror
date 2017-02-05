0310d0a55e33936dcbba72c77a4f91baf59ff25b
Derpy Hooves <bestpony@equestria.mlp>
2017-02-05 16:54:02 -0700

    Derpy is best pony. No exceptions.

--- fan/trot.php
+++ fan/trot.php
@@ -13,89 +13,63 @@
                             <th width="85">Info</th>
                         </tr>
                         <tr>
-                            <td height="20">TrotMania Chrystalize Program</td>
-                            <td> <a href="https://mega.co.nz/#!TkYhDIKT!S-LF2nEsqLCEiC6CVOMGwHV0WVRgTH8K6naQmnhVt8w">1</a> <a href="http://filecloud.io/ec8hao9g">2</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania Chrystalize Program.zip">1</a></td>
-                            <td><a href="/torrents/TrotMania Chrystalize Program.zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/64E5B2B65A3B6B967B01FD0704A8EE08B4E375ED.torrent">3</a> <a href="http://torrage.com/torrent/64E5B2B65A3B6B967B01FD0704A8EE08B4E375ED.torrent">4</a> <a href="http://torcache.net/torrent/64E5B2B65A3B6B967B01FD0704A8EE08B4E375ED.torrent">5</a> <a href="magnet:?xt=urn:btih:64E5B2B65A3B6B967B01FD0704A8EE08B4E375ED">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot1.php" onclick="return popitup('/fan/popup/trotmania/trot1.php')">READ ME</a></td>
+                            <td height="20">TrotMania - Rhythm is Magic</td>
+                            <td> <a href="https://mega.nz/#!lIsWiTZQ!TIKXlpcwvH9X44Iqur8PZfMRxVI-Ahn5h1wD-TfUKro">1</a> 2 3</td>
+                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania_Rhythm_is_Magic.zip">1</a></td>
+                            <td><a href="/fan/torrents/trotmania/TrotMania_Rhythm_is_Magic.zip.torrent">1</a> 2 3 <a href="http://thetorrent.org/torrent/B1EB8F6C686661278771289899DC383F02D30F58.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:B1EB8F6C686661278771289899DC383F02D30F58">6</a> </td>
+                            <td>Trotmania</td>
                         </tr>
                         <tr>
-                            <td height="20">TrotMania Rhythm is Magic Pack (Part 1)</td>
-                            <td><a href="https://mega.co.nz/#!P1Yj1a5C!WGFH5VsVo6dE1phvppWq_AgK1z5Y4fp1PPi73drjd1M">1</a> <a href="http://filecloud.io/seudw783">2</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania Rhythm is Magic Pack (Part 1).zip">1</a></td>
-                            <td><a href="/torrents/TrotMania Rhythm is Magic Pack (Part 1).zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/BBC38F1B427A21D554A2346CC701026C87EC794F.torrent">3</a> <a href="http://torrage.com/torrent/BBC38F1B427A21D554A2346CC701026C87EC794F.torrent">4</a> <a href="http://torcache.net/torrent/BBC38F1B427A21D554A2346CC701026C87EC794F.torrent">5</a> <a href="magnet:?xt=urn:btih:BBC38F1B427A21D554A2346CC701026C87EC794F">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot2.php" onclick="return popitup('/fan/popup/trotmania/trot2.php')">READ ME</a></td>
+                            <td height="20">TrotMania - Chrystalize</td>
+                            <td> <a href="https://mega.nz/#!1UdxRJKS!ZJ5nUOr3Xz8JHeqMpGJsyMdwpfNFzv_mbSUb1ckn23E">1</a> 2 3</td>
+                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania_Chrystalize.zip">1</a></td>
+                            <td><a href="/fan/torrents/trotmania/TrotMania_Chrystalize.zip.torrent">1</a> 2 3 <a href="http://thetorrent.org/torrent/0E0D7FE2ED8E3ED5FD51E9923966BEB83541F361.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:0E0D7FE2ED8E3ED5FD51E9923966BEB83541F361">6</a> </td>
+                            <td>Trotmania</td>
                         </tr>
                         <tr>
-                            <td height="20">TrotMania Rhythm is Magic Pack (Part 2)</td>
-                            <td><a href="https://mega.co.nz/#!C1wmnAwa!ayzxAS9Nl9m7FQ1juIUIFgqgmiUXTuLm0ENyMIyanqE">1</a> <a href="http://filecloud.io/h5lj7isb">2</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania Rhythm is Magic Pack (Part 2).zip">1</a></td>
-                            <td><a href="/torrents/TrotMania Rhythm is Magic Pack (Part 2).zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/BB96267E7C7FD2CA9D62C2387F79299C4A92D960.torrent">3</a> <a href="http://torrage.com/torrent/BB96267E7C7FD2CA9D62C2387F79299C4A92D960.torrent">4</a> <a href="http://torcache.net/torrent/BB96267E7C7FD2CA9D62C2387F79299C4A92D960.torrent">5</a> <a href="magnet:?xt=urn:btih:BB96267E7C7FD2CA9D62C2387F79299C4A92D960">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot3.php" onclick="return popitup('/fan/popup/trotmania/trot3.php')">READ ME</a></td>
+                            <td height="20">TrotMania - Aeternum Obscurum</td>
+                            <td> <a href="https://mega.nz/#!MFsgVRxI!t8hGhSr7hz4x93la4RoGewolPYs4a97EqF0XHaXaclQ">1</a> 2 3</td>
+                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania_Aeternum_Obscurum.zip">1</a></td>
+                            <td><a href="/fan/torrents/trotmania/TrotMania_Aeternum_Obscurum.zip.torrent">1</a> 2 3 <a href="http://thetorrent.org/torrent/8F3EB2C5AE718BD5DC6A12EEC09154BD1DE0564B.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:8F3EB2C5AE718BD5DC6A12EEC09154BD1DE0564B">6</a></td>
+                            <td>Trotmania</td>
+                        </tr>
+
+                        <tr>
+                            <td height="20">TrotMania - Euphorius</td>
+                            <td> <a href="https://mega.nz/#!RckW1KZS!_DNcKidi54MfxLwKChZWTzQoVZ9GlgSpUnmlJrpkj3s">1</a> 2 3</td>
+                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania_Euphorius-Payload_1_Package.zip">1</a></td>
+                            <td><a href="/fan/torrents/trotmania/TrotMania_Euphorius-Payload_1_Package.zip.torrent">1</a> 2 3 <a href="http://thetorrent.org/torrent/A2F206B4856CC09D04F773613157867CDFD6CF53.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:A2F206B4856CC09D04F773613157867CDFD6CF53">6</a> </td>
+                            <td>Trotmania</td>
                         </tr>
                         <tr>
-                            <td height="20">TrotMania Extra 4 (Part 1)</td>
-                            <td><a href="https://mega.co.nz/#!CwpEUJLA!JKqZOn4B38V-3ZmTMLe7q0zByAUU2lyPB0hVP3Ak7ug">1</a> <a href="http://filecloud.io/fpgq73kw">2</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania Extra 4 (Part 1).zip">1</a></td>
-                            <td><a href="/torrents/TrotMania Extra 4 (Part 1).zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/57098AFC3634F8CAE95431D179C9BFC3F68D799B.torrent">3</a> <a href="http://torrage.com/torrent/57098AFC3634F8CAE95431D179C9BFC3F68D799B.torrent">4</a> <a href="http://torcache.net/torrent/57098AFC3634F8CAE95431D179C9BFC3F68D799B.torrent">5</a> <a href="magnet:?xt=urn:btih:57098AFC3634F8CAE95431D179C9BFC3F68D799B">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot4.php" onclick="return popitup('/fan/popup/trotmania/trot4.php')">READ ME</a></td>
+                            <td height="20">TrotMania - Euphorius (Songs Only)</td>
+                            <td> <a href="https://mega.nz/#!ZIEFGRCB!MN2_d2KMukQy5ySDmRVWR-8lj_yCbdWwP_8Vqg5iMxA">1</a> 2 3</td>
+                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania_Euphorius-Songs_Only.zip">1</a></td>
+                            <td><a href="/fan/torrents/trotmania/TrotMania_Euphorius-Songs_Only.zip.torrent">1</a> 2 3 <a href="http://thetorrent.org/torrent/835B07B17E7D21E6302D6531205C5850198A0EB4.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:835B07B17E7D21E6302D6531205C5850198A0EB4">6</a></td>
+                            <td>Songs Only</td>
                         </tr>
                         <tr>
-                            <td height="20">TrotMania Extra 4 (Part 2)</td>
-                            <td><a href="https://mega.co.nz/#!b1BiRKbQ!J4bczG0Zaj4D-5Fp-_KGbQ_pjMp8GxJ1VeyH1JqcqLA">1</a> <a href="http://filecloud.io/inq023oc">2</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania Extra 4 (Part 2).zip">1</a></td>
-                            <td><a href="/torrents/TrotMania Extra 4 (Part 2).zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/3D12173A9A85C558DC7188025EA866A6AB3594DB.torrent">3</a> <a href="http://torrage.com/torrent/3D12173A9A85C558DC7188025EA866A6AB3594DB.torrent">4</a> <a href="http://torcache.net/torrent/3D12173A9A85C558DC7188025EA866A6AB3594DB.torrent">5</a> <a href="magnet:?xt=urn:btih:3D12173A9A85C558DC7188025EA866A6AB3594DB">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot5.php" onclick="return popitup('/fan/popup/trotmania/trot5.php')">READ ME</a></td>
+                            <td height="20">TrotMania - Euphorius (Theme Only)</td>
+                            <td> <a href="https://mega.nz/#!NJ0SnYLK!Gw1dDCPlKJJwi3fK9HJsDv0VIR_eO8ntx4oRZWL4fEc">1</a> 2 3</td>
+                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania_Euphorius-Theme_Only.zip">1</a> <a href="https://yaycdn.de/ypfan/trotmania/TrotMania_Euphorius-Theme_Only.zip">2</a></td>
+                            <td><a href="/fan/torrents/trotmania/TrotMania_Euphorius-Theme_Only.zip.torrent">1</a> 2 3 <a href="http://thetorrent.org/torrent/8CB4B51E987B2705EBBFA709454F68AD48C70813.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:8CB4B51E987B2705EBBFA709454F68AD48C70813">6</a>  </td>
+                            <td>Theme Only</td>
                         </tr>
                         <tr>
-                            <td height="20">TrotMania Extra 5</td>
-                            <td><a href="https://mega.co.nz/#!OtIkETYC!WugT6BxZ7Q9y0TfMMqR4HxR6mgdptgrhEfTnUyDJjB0">1</a> <a href="http://filecloud.io/0qpgtmo3">2</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania Extra 5.zip">1</a></td>
-                            <td><a href="/torrents/TrotMania Extra 5.zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/6F603BA3224075D56AA50275C6A7D007C300B475.torrent">3</a> <a href="http://torrage.com/torrent/6F603BA3224075D56AA50275C6A7D007C300B475.torrent">4</a> <a href="http://torcache.net/torrent/6F603BA3224075D56AA50275C6A7D007C300B475.torrent">5</a> <a href="magnet:?xt=urn:btih:6F603BA3224075D56AA50275C6A7D007C300B475">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot6.php" onclick="return popitup('/fan/popup/trotmania/trot6.php')">READ ME</a></td>
+                            <td height="20">TrotMania - Euphorius (Noteskins Only)</td>
+                            <td> <a href="https://mega.nz/#!UE9DjKrY!mcDNt6PrIDqRoA9BKucSzxKlx3e1Xao8zsd7DBpHwBg">1</a> 2 3</td>
+                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania_Euphorius-Noteskins_Only.zip">1</a> <a href="https://yaycdn.de/ypfan/trotmania/TrotMania_Euphorius-Noteskins_Only.zip">2</a></td>
+                            <td><a href="/fan/torrents/trotmania/TrotMania_Euphorius-Noteskins_Only.zip.torrent">1</a> 2 3 <a href="http://thetorrent.org/torrent/E2E8A834EB4CB6B08CB973CB53A7735DCF3D917E.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:E2E8A834EB4CB6B08CB973CB53A7735DCF3D917E">6</a> </td>
+                            <td>Noteskins Only</td>
                         </tr>
                         <tr>
-                            <td height="20">TrotMania Extra 6</td>
-                            <td><a href="https://mega.co.nz/#!z4hD0awC!JDy8LQEbqpl9DikQZq4fXkUHkF1l6Ta5KnnFEFdBZ8c">1</a> <a href="http://filecloud.io/9ayjn64m">2</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania Extra 6.zip">1</a></td>
-                            <td><a href="/torrents/TrotMania Extra 6.zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/914F71E0525215A505881B3271A395ED86EB953A.torrent">3</a> <a href="http://torrage.com/torrent/914F71E0525215A505881B3271A395ED86EB953A.torrent">4</a> <a href="http://torcache.net/torrent/914F71E0525215A505881B3271A395ED86EB953A.torrent">5</a> <a href="magnet:?xt=urn:btih:914F71E0525215A505881B3271A395ED86EB953A">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot7.php" onclick="return popitup('/fan/popup/trotmania/trot7.php')">READ ME</a></td>
+                            <td height="20">TrotMania - Euphorius (Courses Only)</td>
+                            <td> <a href="https://mega.nz/#!lZ1HSbIL!YxzuAc0YEIvA2uwn4vPQ8Gs5kmArZy31JBzuBw539lA">1</a> 2 3</td>
+                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania_Euphorius-Courses_Only.zip">1</a> <a href="https://yaycdn.de/ypfan/trotmania/TrotMania_Euphorius-Courses_Only.zip">2</a></td>
+                            <td><a href="/fan/torrents/trotmania/TrotMania_Euphorius-Courses_Only.zip.torrent">1</a> 2 3 <a href="http://thetorrent.org/torrent/3E361F747396A382C91320D2E35A8BF592B23E00.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:3E361F747396A382C91320D2E35A8BF592B23E00">6</a> </td>
+                            <td>Courses Only</td>
                         </tr>
-                        <tr>
-                            <td height="20">DanceDanceRevolution Pack (Part 1)</td>
-                            <td><a href="https://mega.co.nz/#!u1hwmaAZ!M6vbKmbm9ruzAIaDODM-aldaOYpGZ_pF2PtwAQxfw5A">1</a> <a href="http://filecloud.io/j6zpqb2i">2</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/DanceDanceRevolution Pack (Part 1).zip">1</a></td>
-                            <td><a href="/torrents/DanceDanceRevolution Pack (Part 1).zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/1425DF22B33DFB395B6357E67234AEB4757AB03A.torrent">3</a> <a href="http://torrage.com/torrent/1425DF22B33DFB395B6357E67234AEB4757AB03A.torrent">4</a> <a href="http://torcache.net/torrent/1425DF22B33DFB395B6357E67234AEB4757AB03A.torrent">5</a> <a href="magnet:?xt=urn:btih:1425DF22B33DFB395B6357E67234AEB4757AB03A">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot8.php" onclick="return popitup('/fan/popup/trotmania/trot8.php')">READ ME</a></td>
-                        </tr>
-                        <tr>
-                            <td height="20">DanceDanceRevolution Pack (Part 2)</td>
-                            <td><a href="https://mega.co.nz/#!3xg3SQCJ!cGsRJxwEpGQxDY7E0OVcz1WsgQ1uMAoFeg6-bYneX-g">1</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/DanceDanceRevolution Pack (Part 2).zip">1</a></td>
-                            <td><a href="/torrents/DanceDanceRevolution Pack (Part 2).zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/0BAA6D0776C992E3C4C7971F1C399DD71F5FA70E.torrent">3</a> <a href="http://torrage.com/torrent/0BAA6D0776C992E3C4C7971F1C399DD71F5FA70E.torrent">4</a> <a href="http://torcache.net/torrent/0BAA6D0776C992E3C4C7971F1C399DD71F5FA70E.torrent">5</a> <a href="magnet:?xt=urn:btih:0BAA6D0776C992E3C4C7971F1C399DD71F5FA70E">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot9.php" onclick="return popitup('/fan/popup/trotmania/trot9.php')">READ ME</a></td>
-                        </tr>
-                        <tr>
-                            <td height="20">TrotMania Extra 7</td>
-                            <td><a href="https://mega.co.nz/#!9U91kIQR!udHWMNnWjPCWkVbz77CGK53FW3Eh0BgM30Q_0_vc9Xc">1</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania Extra 7.zip">1</a></td>
-                            <td><a href="/torrents/TrotMania Extra 7.zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/104F0C3CF225EBEA80C516EB3D87EF212E225D3B.torrent">3</a> <a href="http://torrage.com/torrent/104F0C3CF225EBEA80C516EB3D87EF212E225D3B.torrent">4</a> <a href="http://torcache.net/torrent/104F0C3CF225EBEA80C516EB3D87EF212E225D3B.torrent">5</a> <a href="magnet:?xt=urn:btih:104F0C3CF225EBEA80C516EB3D87EF212E225D3B">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot10.php" onclick="return popitup('/fan/popup/trotmania/trot10.php')">READ ME</a></td>
-                        </tr>
-                        <tr>
-                            <td height="20">TrotMania Extra 8</td>
-                            <td><a href="https://mega.co.nz/#!UMEjhZQK!b2VDaKbj3zGdf327MhkY-tjTAyXMZ_0Mq8K3b9FMtog">1</a> <a href="http://filecloud.io/14qtahz6">2</a> <a href="http://uploadhero.co/dl/37a7a4fc">3</a></td>
-                            <td><a href="https://yp.coco-pommel.org/ypfan/trotmania/TrotMania Extra 8.zip">1</a></td>
-                            <td><a href="/torrents/TrotMania Extra 8.zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/AC468A8F1D36A1E9F65FE4D1329DD131C4101A37.torrent">3</a> <a href="http://torrage.com/torrent/AC468A8F1D36A1E9F65FE4D1329DD131C4101A37.torrent">4</a> <a href="http://torcache.net/torrent/AC468A8F1D36A1E9F65FE4D1329DD131C4101A37.torrent">5</a> <a href="magnet:?xt=urn:btih:AC468A8F1D36A1E9F65FE4D1329DD131C4101A37">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot11.php" onclick="return popitup('/fan/popup/trotmania/trot11.php')">READ ME</a></td>
-                        </tr>
-                        <tr>
-                            <td height="20">TrotMania Chrystalize Append</td>
-                            <td><a href="https://mega.co.nz/#!NRh1XRwI!ADZ-hmZs83XkasgzAXiwo9Ac1QQP0AbmWduH6NfssHQ">1</a> <a href="http://filecloud.io/pdbe9x3m">2</a> <a href="http://uploadhero.co/dl/cc188065">3</a></td>
-                            <td>&nbsp;</td>
-                            <td><a href="/torrents/TrotMania Chrystalize Append.zip.torrent">1</a> 2 <a href="http://zoink.it/torrent/25382AB184F9A69DFF01415CE6013E898AE55AEC.torrent">3</a> 4 <a href="http://torcache.net/torrent/25382AB184F9A69DFF01415CE6013E898AE55AEC.torrent">5</a> <a href="magnet:?xt=urn:btih:25382AB184F9A69DFF01415CE6013E898AE55AEC">6</a></td>
-                            <td><a href="/fan/popup/trotmania/trot12.php" onclick="return popitup('/fan/popup/trotmania/trot12.php')">READ ME</a></td>
-                        </tr>
+
                     </tbody>
                 </table>
                 <p><a href="http://trotmania.blogspot.com/">Go to TrotMania official Website !</a></p>
