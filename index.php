a5c8b72a20ca8b97cede1a6d8bf6cd0e0fbacf41
bestpony@ponyville.eq <derpy@7dd18f2b56a5>
2017-03-31 22:08:01 +0000

    Derpy is best pony. No exceptions.

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<!--#include file="/resources/header.php" -->
-            <p style="font-weight: bold">GIT MIRROR OWNER: IF YOUR MIRROR IS BROKEN, PLEASE ENABLE SSI, <a href="https://git.yayponies.pw/upgradetossi">INSTRUCTION HERE!</a>
-            </p><p style="font-weight: bold">Welcome to Yay Ponies! Do navigate through the repository with the various links above!</p>
-            <p>
-                Thanks to the break between episode 21 and episode 22, we was able to do a little cleanup on the site !
-                Now the two official mirrors support HTTPS, with a trusted certificate, so you don't have to bypass the warning !
-                <br /><br />
-                We also uploaded previous ponies to mega.co.nz, now everypony is downloadable from there !
-                <br />
-                <br />
-                And finally, we removed the 720p links to newlunarrepublic (they will be removed from their server real soon)
-                and replaced them by 1080p links.
-            </p>
-            <p> MUFFINS !<br />YayPonies Staff</p>
-        
-    </body>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>YP Fish</title>
+</head>
+<body>
+<p><a href="poni.php">Go back to YP</a> - Hosted on Windows FW 3.11
+</p>
+<iframe src=http://ypvm.ns01.us width="100%" style="height: 100em"></iframe>
+</body>
 </html>
