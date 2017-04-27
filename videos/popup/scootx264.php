98f015983af35fdf15331e9edc9e93d6abb46dae
bestpony@ponyville.eq <derpy@7dd18f2b56a5>
2017-04-27 07:38:01 +0000

    Derpy is best pony. No exceptions.

--- videos/popup/scootx264.php
+++ videos/popup/scootx264.php
@@ -1,7 +1,6 @@
 <!--#include file="/resources/popup/header.php" -->
 <p>Scootaloo Remastered x264 are a remaster of the best available version of pony by Scootaloo.    </p>
-<p>Quality is better than iTunes raw thanks to Scootaloo color restoration process.<br />
-  Quality is also better than the x265 Scootaloo right now.
+<p>Quality is better than iTunes raw thanks to Scootaloo color restoration process.
 </p>
 <p> Audio tracks (Both AC3 5.1 and AAC) has been remastered, please use the AAC track on a stereo/laptop setup.</p>
 <p>==========</p>
@@ -13,4 +12,3 @@ There was a correction error which happened in the HEVC which when encoded react
 Some episodes will heavily benefit from being cleaned up, and will be smaller but others will be bigger due to needing bits for the debanding or noise reduction due to things like snow, rain ...<br />
 Many of the episodes were so noisy we had to restore debanding on everything, so thats can cause an issue with some episodes sizes.</p>
 <p>The target size for the video stream is around 655MB, many are coming in lower sizes.</p>
-<p>Right now, we cannot get x265 to match these episodes in quality,  when HEVC hits v2.2, we'll redo these x264 in 265.</p>
