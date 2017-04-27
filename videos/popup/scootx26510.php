84814a4f7e88dec6ce4ac14683e102bd1b2f25e2
bestpony@ponyville.eq <derpy@7dd18f2b56a5>
2017-04-27 03:33:01 +0000

    Derpy is best pony. No exceptions.

---
+++ videos/popup/scootx26510.php
@@ -0,0 +1,8 @@
+<!--#include file="/resources/popup/header.php" -->
+<p>Scootaloo Definitive Edition HEVC are x265 main10p encodes of the best available version by Scootaloo.    </p>
+<p>Quality is better than other scootaloo group release due the format capacity! </p>
+<p> Audio tracks (Both AC3 5.1 and AAC) has been remastered, please use the AAC track on a stereo/laptop setup.</p>
+<p> If you encounter frame drop, slow video playing, or simply can't play the file, please make sure your device is compatible with HEVC/x265 videos.    </p>
+<p>You need MPC-HC 1.7.1+ or VLC 2.2.0+ on computers    Most of the </p>
+<p>Scootaloo eps use many fixes to bring the best version in HEVC.</p>
+</div>
