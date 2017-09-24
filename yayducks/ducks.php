50b5918ebf376ac1ccffbab0a936f160a35f6fe0
bestpony@ponyville.eq <derpy@7dd18f2b56a5>
2017-09-24 19:39:01 +0000

    Derpy is best pony. No exceptions.

--- yayducks/ducks.php
+++ yayducks/ducks.php
@@ -4,7 +4,7 @@
 <center>
     <p>Welcome to the download page for DuckTales. For now, the files are only available by torrent.</p>
     <p>If you are impatient and want to get new files as soon as they are ready, check out our <a href="https://ponychat.net/webchat/?autojoin=%23yayponies">IRC chan</a>, since website is somtimes updated several hours after the actual file release.</p>
-    <p>Subs labeled "RAW" are extracted from the iTunes files themself, those labeled "FAN" are prepared by a fan and are better made (thank you Henke34!)</p>
+    <p>Subs labeled "RAW" are extracted from the iTunes files themself, those labeled "FAN" are prepared by a fan and are better made (thank you Henke37!)</p>
     <p>Season 1 iTunes RAW :</p>
     <table width="752" align="center">
         <tbody>
@@ -30,7 +30,7 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x02.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/639394C0574D10A36829BCF2EA3A36E1DEB8113A.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:639394C0574D10A36829BCF2EA3A36E1DEB8113A">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-fan/YP-DT-1R-01x02.srt">RAW</a></td>
+                <td><a href="subtitles-raw/YP-DT-1R-01x02.srt">RAW</a></td>
             </tr>
             <tr>
                 <td height="20"><a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6252388&lid=7">Episode 3</a> (<a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6252388&lid=7">The Great Dime Chase!</a>)</td>
@@ -38,7 +38,7 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x03.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/FF2732E83052B5E465A982878EBE7894E5F0EEBE.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:FF2732E83052B5E465A982878EBE7894E5F0EEBE">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-fan/YP-DT-1R-01x03.srt">RAW</a></td>
+                <td><a href="subtitles-raw/YP-DT-1R-01x03.srt">RAW</a></td>
             </tr>
         </tbody>
     </table>
