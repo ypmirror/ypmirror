0d0b746261d366c7d2128667b44f0be32baa5e19
bestpony@ponyville.eq <derpy@7dd18f2b56a5>
2018-04-09 21:07:01 +0000

    Derpy is best pony. No exceptions.

--- yayducks/ducks.php
+++ yayducks/ducks.php
@@ -22,7 +22,7 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x01.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/A7940AF3C3EDED8B8A13F66F33F4E9E8E12AAA21.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:A7940AF3C3EDED8B8A13F66F33F4E9E8E12AAA21">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-fan/YP-DT-1R-01x01.srt">FAN</a></td>
+                <td><a href="subtitles/YP-DT-1R-01x01.srt">FAN</a></td>
             </tr>
             <tr>
                 <td height="20"><a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6252387&lid=7">Episode 2</a> (<a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6252387&lid=7">Daytrip of Doom!</a>)</td>
@@ -30,7 +30,7 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x02.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/639394C0574D10A36829BCF2EA3A36E1DEB8113A.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:639394C0574D10A36829BCF2EA3A36E1DEB8113A">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-raw/YP-DT-1R-01x02.srt">RAW</a></td>
+                <td><a href="subtitles/YP-DT-1R-01x02.srt">RAW</a></td>
             </tr>
             <tr>
                 <td height="20"><a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6252388&lid=7">Episode 3</a> (<a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6252388&lid=7">The Great Dime Chase!</a>)</td>
@@ -38,7 +38,7 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x03.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/FF2732E83052B5E465A982878EBE7894E5F0EEBE.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:FF2732E83052B5E465A982878EBE7894E5F0EEBE">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-raw/YP-DT-1R-01x03.srt">RAW</a></td>
+                <td><a href="subtitles/YP-DT-1R-01x03.srt">RAW</a></td>
             </tr>
             <tr>
                 <td height="20"><a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6253688&lid=7">Episode 4</a> (<a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6253688&lid=7">The The Beagle Birthday Massacre!</a>)</td>
@@ -46,7 +46,7 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x04.mkv.torrent">1</a> 2 3 4 5 <a href="magnet:?xt=urn:btih:8b2e7646481a28b87419894d04fe7b402c5576c0">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-raw/YP-DT-1R-01x04.srt">RAW</a></td>
+                <td><a href="subtitles/YP-DT-1R-01x04.srt">RAW</a></td>
             </tr>
             <tr>
                 <td height="20"><a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6345315&lid=7">Episode 5</a> (<a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6345315&lid=7">The 	Terror of the Terra-Firmians</a>)</td>
@@ -54,7 +54,7 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x05.mkv.torrent">1</a> 2 3 4 5 <a href="magnet:?xt=urn:btih:bd600c8bc59e6e7e6b64b214c9dbc4e00896ada3">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-raw/YP-DT-1R-01x05.srt">RAW</a></td>
+                <td><a href="subtitles/YP-DT-1R-01x05.srt">RAW</a></td>
             </tr>
             <tr>
                 <td height="20"><a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6345317&lid=7">Episode 6</a> (<a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6345317&lid=7">The House of the Lucky Gander!</a>)</td>
@@ -62,7 +62,7 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x06.mkv.torrent">1</a> 2 3 4 5 <a href="magnet:?xt=urn:btih:07f0ed91f5732635c2edc71c4ab23733f1456315">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-raw/YP-DT-1R-01x06.srt">RAW</a></td>
+                <td><a href="subtitles/YP-DT-1R-01x06.srt">RAW</a></td>
             </tr>
             <tr>
                 <td height="20"><a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6331833&lid=7">Episode 7</a> (<a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6331833&lid=7">The Infernal Internship of Mark Beaks</a>)</td>
@@ -70,7 +70,7 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x07.mkv.torrent">1</a> 2 3 4 5 <a href="magnet:?xt=urn:btih:8bc3054a2804ba29387b46e83474300d707bbd07">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-raw/YP-DT-1R-01x07.srt">RAW</a></td>
+                <td><a href="subtitles/YP-DT-1R-01x07.srt">RAW</a></td>
             </tr>
             <tr>
                 <td height="20"><a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6331835&lid=7">Episode 8</a> (<a href="https://www.thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6331835&lid=7">The Living Mummies of Toth-Ra!</a>)</td>
@@ -78,7 +78,16 @@
                 <td>nope</td>
                 <td><a href="torrents/YP-DT-1R-01x08.mkv.torrent">1</a> 2 3 4 5 <a href="magnet:?xt=urn:btih:758ee66e1074b55770a9e0598052a69f61f7fb13">6</a></td>
                 <td>YP iTunes 1080P</td>
-                <td><a href="subtitles-raw/YP-DT-1R-01x08.srt">RAW</a></td>
+                <td><a href="subtitles/YP-DT-1R-01x08.srt">RAW</a></td>
+            </tr>
+			            <tr>
+                <td height="20"><a href="http://thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6387496&lid=7">Episodes 9</a> (<a href="http://thetvdb.com/?tab=episode&seriesid=330134&seasonid=716277&id=6387496&lid=7">The Impossible Summit of Mt. Neverrest!</a>)</td>
+                <td>1 2 3</td>
+                <td>nope</td>
+                <td><a href="torrents/YP-DT-1R-01x09.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/e1f66633c5ab76c622a2b27691a5654de7607d1d.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:e1f66633c5ab76c622a2b27691a5654de7607d1d">6</a></td>
+                <td>YP iTunes 1080P</td>
+                <td></td>
+                <td><a href="subtitles/YP-DT-1R-01x09.srt">RAW</a></td>
             </tr>
         </tbody>
     </table>
