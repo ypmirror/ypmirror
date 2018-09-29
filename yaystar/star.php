c07786dbe093d7d654058b82bbd367779dfbd7a8
bestpony@ponyville.eq <derpy@7dd18f2b56a5>
2018-09-29 00:44:02 +0000

    Derpy is best pony. No exceptions.

--- yaystar/star.php
+++ yaystar/star.php
@@ -49,6 +49,143 @@
         <td>YP iTunes 1080P</td>
         <td><a href="subtitles/YP-SV-1R-03x07.srt">Get</a></td>
       </tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6354245&lid=7">Episode 08 & 09(Scent of a Hoodie & Rest in Pudding)</a></td>
+<td><a href="https://mega.nz/#!m2phhARb!OvbsZyqoXXGjAoUFpDcdo9aNT1gZ8BbGJ9zT2ENMLz4">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x08_09.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x08_09.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/7f17207f412306eff4da24b66256a8c5200afa9f.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:7f17207f412306eff4da24b66256a8c5200afa9f">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x08_09.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6354267&lid=7">Episode 10 & 11(Club Snubbed & Stranger Danger)</a></td>
+<td><a href="https://mega.nz/#!HrwQBKiS!jNP_5_UzZ8p0zeO9SRsuJsQOHTMt9l69tUvhBmIxdAk">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x10_11.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x10_11.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/97404729497df71c4277dd77fc046c28381338ef.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:97404729497df71c4277dd77fc046c28381338ef">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x10_11.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6354276&lid=7">Episode 12 & 13(Demoncism & Sophomore Slump)</a></td>
+<td><a href="https://mega.nz/#!6uoHkAAa!RFMNopnDMRF-R0PNYpXqCnVRJCRRxvxdOm5nso38klc">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x12_13.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x12_13.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/76fb505cfec3a76f665c2a08b0e51f77104c9761.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:76fb505cfec3a76f665c2a08b0e51f77104c9761">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x12_13.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6354283&lid=7">Episode 14 & 15(Lint Catcher & Trial by Squire)</a></td>
+<td><a href="https://mega.nz/#!2z4gVSZb!GsiQ3SAQU_pbWod7fP3Ui3fx3AQ5qa2r1EyHvP764pk">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x14_15.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x14_15.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/a4ec941780b5cfcd76eb5fb4abe7e20920824688.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:a4ec941780b5cfcd76eb5fb4abe7e20920824688">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x14_15.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6354286&lid=7">Episode 16 & 17(Princess Turdina & Starfari)</a></td>
+<td><a href="https://mega.nz/#!zjo1FACR!UFFAKeGi5Z7WhVW8Uw-8VxT7ZMhRo6jqvbkTClpluLI">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x16_17.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x16_17.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/455c54c50c4e2ea8683adfc2490a8819963600d8.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:455c54c50c4e2ea8683adfc2490a8819963600d8">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x16_17.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6354291&lid=7">Episode 18 & 19(Sweet Dreams & Lava Lake Beach)</a></td>
+<td><a href="https://mega.nz/#!SupCmAgS!0rW6FAmvHqH8gBnK4KrIswUaeN73RAl895nrq5LXtQE">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x18_19.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x18_19.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/6e7f17d0e352b29743c41b199902358072cfad8a.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:6e7f17d0e352b29743c41b199902358072cfad8a">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x18_19.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6354292&lid=7">Episode 20 & 21(Death Peck & Ponymonium)</a></td>
+<td><a href="https://mega.nz/#!j3xygSSS!8m6KmSa3Oy9UTi4yfe7N2qgFhB6xnJZ6Dr0LQhB-_RE">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x20_21.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x20_21.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/3c4fbca5d87b46e7a820767f98e87ac11e30a3f4.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:3c4fbca5d87b46e7a820767f98e87ac11e30a3f4">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x20_21.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6354295&lid=7">Episode 22 & 23(Night Life & Deep Dive)</a></td>
+<td><a href="https://mega.nz/#!uqxX3CxL!oTNOTPZnCVhAUrEwLiv_6Umi3edpyV77Oz5zDyu0J0w">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x22_23.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x22_23.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/fe61c99761418ca029438de0696a56884c4cdb93.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:fe61c99761418ca029438de0696a56884c4cdb93">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x22_23.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6354297&lid=7">Episode 24 (Monster Bash)</a></td>
+<td><a href="https://mega.nz/#!Lz4nHIDS!8R9drUuqPomxuBn972w4zZktr3ThaFQLeuseypDRSKM">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x24.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x24.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/c9a5404eb7eb952a2baa651abfe28ff6c6ac2663.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:c9a5404eb7eb952a2baa651abfe28ff6c6ac2663">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x24.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6408255&lid=7">Episode 25 & 26(Stump Day & Holiday Spellcial)</a></td>
+<td><a href="https://mega.nz/#!rvwRlIjC!S0-B20fy_lKU8LxV4nMAW3EPhejjMLoA3Dh4L2LMbqQ">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x25_26.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x25_26.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/a907955d020feebf188afd225f3c74702b2d2fde.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:a907955d020feebf188afd225f3c74702b2d2fde">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x25_26.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6507765&lid=7">Episode 27 & 28(The Bogbeast of Boggabah & Total Eclipsa the Moon)</a></td>
+<td><a href="https://mega.nz/#!GuwBXAYL!d2l6hozVQY1LkpLAZtbsjRzYctOJ-pvqSXx2yPJJvnU">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x27_28.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x27_28.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/1e413a4aac8eb1e0e0bc26c07135530718b67356.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:1e413a4aac8eb1e0e0bc26c07135530718b67356">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x27_28.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6547722&lid=7">Episode 29 & 30(Butterfly Trap & Ludo, Where Art Thou?)</a></td>
+<td><a href="https://mega.nz/#!WzpCiCzC!Fl8PBmb_flPz2dhGRh7T5qBqJ86amg3Hb3JkKWO1uXM">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x29_30.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x29_30.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/335890efddcda46232eb72acf0878ef073d9f329.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:335890efddcda46232eb72acf0878ef073d9f329">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x29_30.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6547735&lid=7">Episode 31 & 32(Is Another Mystery & Marco Jr.)</a></td>
+<td><a href="https://mega.nz/#!LzgmGIDA!xqkqGP1gT8LUwZUySd8zNUYULQSSbyTgsm_b_cp5dDw">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x31_32.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x31_32.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/3e9ff1921d145bde0640258c7c5a454ac870b229.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:3e9ff1921d145bde0640258c7c5a454ac870b229">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x31_32.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6547740&lid=7">Episode 33 & 34(Skooled! & Booth Buddies)</a></td>
+<td><a href="https://mega.nz/#!n7x2naIT!_a9WOUWaxSFRCtu_q7Bw0tjILnGbIpsd6Y6rLxDzOYo">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x33_34.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x33_34.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/c85baaa2aa7dd4be85961892d236549d60b74bde.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:c85baaa2aa7dd4be85961892d236549d60b74bde">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x33_34.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6547747&lid=7">Episode 35 & 36(Bam Ui Pati! & Tough Love)</a></td>
+<td><a href="https://mega.nz/#!3jpUTYhC!lnOiIy5cZSM3fjRQamqyFyLd_F-4MyV2J5_ET_EqiL8">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x35_36.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x35_36.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/e1d15c702be9a95b629c4f945a7afd946e0c0a9a.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:e1d15c702be9a95b629c4f945a7afd946e0c0a9a">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x35_36.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6547760&lid=7">Episode 37 (Divide)</a></td>
+<td><a href="https://mega.nz/#!erh2WapD!Go7e8aHw9v8LmjXIDg5tJI0iQkHT4Dqnto2Wh5Uhzew">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x37.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x37.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/1601ac528d8bc325bb863805b2e20f1ab41fa2fe.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:1601ac528d8bc325bb863805b2e20f1ab41fa2fe">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x37.srt">Get</a></td>
+</tr>
+<tr>
+<td><a href="http://thetvdb.com/?tab=episode&seriesid=282994&seasonid=718437&id=6547773&lid=7">Episode 38 (Conquer)</a></td>
+<td><a href="https://mega.nz/#!GzwkzIia!eTZ48Cy9si-vK6QqwmI_x4A01WxUO8kgpLaw5UDrqv4">1</a> 2 3</td>
+<td><a href="https://dl.yayponies.no/YP-SV-1R-03x38.mkv">2</a></td>
+<td><a href="/yaystar/torrents/YP-SV-1R-03x38.mkv.torrent">1</a> 2 3 <a href="http://thetorrent.org/713ffd2639a47bce42f2c54cd00856fdc002639f.torrent">4</a> 5 <a href="magnet:?xt=urn:btih:713ffd2639a47bce42f2c54cd00856fdc002639f">6</a></td>
+<td>YP iTunes 1080P</td>
+<td><a href="subtitles/YP-SV-1R-03x38.srt">Get</a></td>
+</tr>
+
     </tbody>
   </table>
 </center>
